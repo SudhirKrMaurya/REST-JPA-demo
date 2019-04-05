1f72c0c79dc279cb14a79dd9dfb845483cffe28d
sudhir kumar maurya <sudhir14it910@ietlucknow.ac.in>
2019-04-05 16:22:03 +0530

    final demo commit

--- src/main/java/com/jpa/demo/JpaDemo.java
+++ src/main/java/com/jpa/demo/JpaDemo.java
@@ -2,13 +2,17 @@ package com.jpa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JpaDemo {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(JpaDemo.class, args);
-		
+		ConfigurableApplicationContext applicationContext=SpringApplication.run(JpaDemo.class, args);
+		for(String name:applicationContext.getBeanDefinitionNames())
+		{
+			 System.out.println(name);
+		}
 	}
 
 }
